a3ca17fef47976098d248f0e3df7b70e64b59dcc
Harish <hramine@bgsu.edu>
2026-03-10 18:17:05 -0400

    terraform

--- main.tf
+++ main.tf
@@ -167,19 +167,19 @@ resource "aws_route" "database" {
 }
 
 resource "aws_route_table_association" "public" {
-  count = length(count.index)
+  count = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
-  count = length(count.index)
+  count = length(var.private_subnet_cidrs)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "database" {
-  count = length(count.index)
+  count = length(var.database_subnet_cidrs)
   subnet_id      = aws_subnet.database[count.index].id
   route_table_id = aws_route_table.database.id
 }
