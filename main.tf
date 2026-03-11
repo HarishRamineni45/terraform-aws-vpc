ba8a6093df386716f5d7065dd1cb6415fedd4bf2
Harish <hramine@bgsu.edu>
2026-03-11 15:58:00 -0400

    terraform

--- main.tf
+++ main.tf
@@ -186,3 +186,4 @@ resource "aws_route_table_association" "database" {
 
 
 
+
--- peering.tf
+++ peering.tf
@@ -4,6 +4,8 @@ resource "aws_vpc_peering_connection" "default" {
   vpc_id        = aws_vpc.main.id  #Requester VPC
 
   auto_accept   = true
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
   accepter {
     allow_remote_vpc_dns_resolution = true
@@ -19,4 +21,34 @@ resource "aws_vpc_peering_connection" "default" {
             Name = "${var.project}-${var.environment}-default"
         }
     )
+}
+
+resource "aws_route" "public_peering" {
+  count         = var.is_peering_required ? 1 : 0
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id             = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "private_peering" {
+  count         = var.is_peering_required ? 1 : 0
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id             = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "database_peering" {
+  count         = var.is_peering_required ? 1 : 0
+  route_table_id         = aws_route_table.database.id
+  destination_cidr_block = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id             = aws_vpc_peering_connection.default[count.index].id
+}
+
+
+
+resource "aws_route" "default_peering" {
+  count         = var.is_peering_required ? 1 : 0
+  route_table_id         = data.aws_route_tables.default.id
+  destination_cidr_block =  var.vpc_cidr
+  vpc_peering_connection_id             = aws_vpc_peering_connection.default[count.index].id
 }
